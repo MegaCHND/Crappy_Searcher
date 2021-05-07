33b5632c1ce2ad21f12834306664bb780d1a4f76
Chris <chrsdng5@gmail.com>
2021-05-07 11:53:04 -0700

    Made indexer.py This is the file i plan to turn in

--- Testing.py
+++ Testing.py
@@ -1,8 +1,10 @@
 import os
 import re
 import json
+import glob
+import pathlib
 import csv #using csv to read and write to temp invertedIndex since it is much faster
-#import pandas as pd #using pandas to compress duplicates in the inverted Index since it is much faster than csv
+import pandas as pd #using pandas to compress duplicates in the inverted Index since it is much faster than csv
 from functools import reduce
 from bs4 import BeautifulSoup
 from collections import Counter
@@ -13,12 +15,17 @@ from nltk.stem import SnowballStemmer
 CurrUrl = ""
 CurrFilePath = ""
 partialInvertedIndex = {}
+i = 1
+
 #Vars that should never be changed
 CurrDirectory = os.getcwd()
 directory_in_str = '\dev2'
 directory = os.fsencode(CurrDirectory + directory_in_str)
 ps = SnowballStemmer('english')
-tag_names = ["h1","h2","h3","h4","h5","h6","title","p"]
+tag_names = ["h1","h2","h3","h4","h5","h6","title","p","strong"]
+extension = 'csv'
+#os.chdir("\Crappy_Searcher")
+piiFileNameBase = "PartialInvertedIndex"
 
 
 def wordCollector(soup):
@@ -27,8 +34,10 @@ def wordCollector(soup):
     global partialInvertedIndex
     print(CurrFilePath)
     text = ""
-    text = soup.get_text(" ")
-    #tag_list=soup.find_all()
+    tag_list=soup.find_all()
+    for tag in tag_list:
+        if tag.name in tag_names:
+            text = tag.text + " "
     text = re.sub(r'[^a-zA-Z0-9 ]','', text).lower()
     text = remov_duplicates(text)
     for i in text.split():
@@ -68,7 +77,9 @@ def printReport():
 
 
 def WriteToInvertedIndex(pII):
-    with open('InvertedIndex.csv', 'w') as Iindex:
+    global i
+    name = piiFileNameBase+str(i)+".csv"
+    with open(name, 'w') as Iindex:
 
         #declaring the fieldnames for the CSV file
         fieldNames = ['word', 'Urls', 'Filepaths']
@@ -88,9 +99,20 @@ def WriteToInvertedIndex(pII):
             #writing the row
             csvWriter.writerow({'word': word, 'Urls': fileUrlString, 'Filepaths': filePathString})
 
+def combinePartials():
+    global CurrDirectory
+    global i
+    files = list(pathlib.Path(CurrDirectory).glob('*.csv'))              
+    combined_csv = pd.concat([pd.read_csv(f) for f in files ])
+    combined_csv.to_csv( "combined_InvertedIndex.csv", index=False, encoding='utf-8-sig')
+    i = 1
+    
 for subdir, dirs, files in os.walk(directory):
      for file in files:
         openFile(subdir,file)
-WriteToInvertedIndex(partialInvertedIndex)
+        WriteToInvertedIndex(partialInvertedIndex)
+        i+=1
+        if(i >= 20):
+            combinePartials()            
 print("done!")
 #printReport()
---
+++ indexer.py
@@ -0,0 +1,124 @@
+import os
+from collections import defaultdict
+from bs4 import BeautifulSoup
+import re
+import traceback
+import json
+import math
+from nltk.stem import SnowballStemmer #For stemming, use Snowball from nltk
+
+#Since we'll need to rank words for searching later, might as well try to do so now
+#I chose strong, b, bold, h1-3, and title. I didn't include h4-6 b/c the text shrinks back to normal the closer you get to 6
+#p isn't special b/c the prof didn't include it in his list in the assignment =p
+SPECIAL_TAG_FACTORS = {
+    "strong": 1.2,
+    "b": 1.2,
+    "bold": 1.2,
+    "h1": 1.4,
+    "h2": 1.3,
+    "h3": 1.25,
+    "title": 1.5,
+}
+
+#Made classes for the inverted index to use
+#Posting contains the tf score as well as the specail tags used (if the word was in any)
+class Posting:
+    def __init__(self):
+        self.tf = 0
+        self.special_tags = set()
+        
+#Data contains the posting as well as the idf score for the word
+class Data:
+    def __init__(self):
+        self.idf = 0
+        self.postings = defaultdict(Posting)
+        
+#inverted index is a data scructure so I made it one unlike what we had before
+#I think I got every important point we need
+class InvertedIndex:
+    def __init__(self):
+        self.dictionary = defaultdict(Data)
+        self.num_doc_ids = 0
+    def __getitem__(self, term):
+        return self.dictionary[term]
+    def __setitem__(self, term, data):
+        self.dictionary[term] = data
+    def get_number_of_terms(self):
+        return len(self.dictionary.keys())
+    def items(self):
+        return self.dictionary.items()
+    def get_Full_Index(self, outfile):
+        j = dict()
+        for term, data in self.dictionary.items():
+            j[term] = dict()
+            j[term]["idf"] = data.idf
+            j[term]["postings"] = dict()
+            for docID, posting in data.postings.items():
+                j[term]["postings"][docID] = dict()
+                j[term]["postings"][docID]["tf"] = posting.tf
+                j[term]["postings"][docID]["special_tags"] = list(posting.special_tags)
+        json.dump(j, outfile, indent = 4)
+    def print_report(self, outfile):
+        outfile.write("Words Found \n")
+        outfile.write(str(self.get_number_of_terms()))
+        outfile.write("\nNumber of indexed documents \n")
+        outfile.write(str(self.num_doc_ids))
+
+def tokenize(webpage, docID):
+    print("tokenizing " + docID)
+    global index
+    global ps
+    soup = BeautifulSoup(webpage, 'lxml')
+    #Notice, I tokenize every word in the soup like we used to. I can change it to tokenize only in the special tags tho
+    text = soup.get_text()
+    tokens = re.split('\W+', text.lower().strip())            
+    for token in tokens:
+        if token != "":
+            token = ps.stem(token)
+            index[token].postings[docID].tf += 1
+            
+    #Here, I'm not tokenizing again but adding the specail tags to the corresponding words
+    special_tags = soup.find_all(["strong", "b", "bold", "h1", "h2", "h3", "title"])
+    for special_tag in special_tags:
+        special_tag_content = special_tag.get_text().strip()
+        tokens = re.split('\W+', special_tag_content)
+        for token in tokens:
+            if token != "":
+                token = ps.stem(token)
+                index[token].postings[docID].special_tags.add(special_tag.name)
+
+def openFile(subdir,file):
+    global CurrFilePath
+    CurrFilePath = os.fsdecode(os.path.join(subdir, file))
+    OpenedFile = open(CurrFilePath, "r")
+    JsonContent = json.load(OpenedFile)
+    HtmlContent = JsonContent["content"]
+    CurrUrl = JsonContent["url"]
+    tokenize(HtmlContent, CurrUrl)
+
+index = InvertedIndex();
+CurrDirectory = os.getcwd()
+directory_in_str = '\developer'
+directory = os.fsencode(CurrDirectory + directory_in_str)
+CurrFilePath = ""
+ps = SnowballStemmer('english')
+
+try:
+    for subdir, dirs, files in os.walk(directory):
+     for file in files:
+        index.num_doc_ids += 1
+        openFile(subdir,file)
+        index.num_doc_ids += 1
+    for term, data in index.items():
+        #after the index is made, I go back and calculate the idf vals for every word (technically stem)
+        data.idf = index.num_doc_ids/float(len(data.postings))
+except:
+    traceback.print_exc()
+    
+#Here I print stuff for M1 report
+reportFile = open("report.txt" , "w") 
+indexFile = open("index.json", "w")   
+index.get_Full_Index(indexFile)
+index.print_report(reportFile)
+reportFile.close()
+indexFile.close()
