6fe1c37c0cf4019812dff31de9f4768ec2c0671c
santalsania <62778748+santalsania@users.noreply.github.com>
2021-05-21 14:33:14 -0700

    updated index of indexes maker

--- ijsonTEst.py
+++ ijsonTEst.py
@@ -1,7 +1,21 @@
 import ijson
+def index_maker(file):
+    word_location={}
+    reader = open(file)
+    length_start=0
+    for lines in reader:
+        line=lines.split(",")[0]
+        line2=line.replace("[","")
+        line2=line2.replace("\"","")
+        word_location[line2]=length_start
+        length_start+=len(lines)+1
+    return word_location
+dictionary1=index_maker("ParIndex0.json")
+print(dictionary1["irvin"])
 
-reader = open('ParIndex0.json')
-objects = ijson.parse(reader)
-for line in objects:
-    if("artifici" in line[0]):
-        print(line[0], line[1])
+
+
+
+
+
+    
